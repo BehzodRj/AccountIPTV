a452091d110a582be5bb50b8a26b5041393895ec
BehzodRj <behzodfarishta@gmail.com>
2022-08-02 15:14:35 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChannelPageComponent } from './channel-page/channel-page.component';
+import { ClientPageComponent } from './client-page/client-page.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChannelPageComponent },
+  { path: '/client', component: ClientPageComponent },
+  { path: '/header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientPageComponent } from './client-page/client-page.component';
+import { ChannelPageComponent } from './channel-page/channel-page.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientPageComponent,
+    ChannelPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
