52285dfef1ac6df0c7ac34a6b4c292c713b85e1d
Vafo1212 <106663890+Vafo1212@users.noreply.github.com>
2022-08-05 17:27:07 +0500

    some change

--- src/app/edit-category-page/edit-category-page.component.ts
+++ src/app/edit-category-page/edit-category-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-category-page',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-category-page.component.scss']
 })
 export class EditCategoryPageComponent implements OnInit {
-
+  addCotegoryForm!:FormGroup
   constructor() { }
 
   ngOnInit(): void {
-  }
+
+    this.addCotegoryForm = new FormGroup({
+      id: new FormControl('', Validators.required),
+      main: new FormControl('', Validators.required),
+  })
+  const addCotegoryFormData = {...this.addCotegoryForm.value}
+  console.log(addCotegoryFormData);
+}
 
 }
